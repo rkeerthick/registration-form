820c4647f6d79d106a5dbca03beb0c35936f6a53
rkeerthick <75026995+rkeerthick@users.noreply.github.com>
2023-03-31 09:48:44 +0530

    Update ConnectionUtil.java

--- Registration form/src/main/java/com/util/ConnectionUtil.java
+++ Registration form/src/main/java/com/util/ConnectionUtil.java
@@ -8,9 +8,9 @@ public class ConnectionUtil {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException
 	{
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/regform", "root", "#Keer2003#");
-		if(con != null) System.out.println("not connected");
-		else System.out.println("connected");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/regform", "root", "abcdefg");
+		if(con != null) System.out.println("Connected");
+		else System.out.println("Not Connected");
 		return con;
 	}
 }
